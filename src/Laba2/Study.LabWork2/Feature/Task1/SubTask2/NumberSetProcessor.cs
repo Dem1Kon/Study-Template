483a0eba8a3c689d7fb6d45f22a4f9deb99ed0e8
Demian <konoplev.dm.06@gmail.com>
2026-05-09 09:49:49 +0300

    [feat] lab2_1.2 without tests

--- src/Laba2/Study.LabWork2/Feature/Task1/SubTask2/NumberSetProcessor.cs
+++ src/Laba2/Study.LabWork2/Feature/Task1/SubTask2/NumberSetProcessor.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using Study.LabWork2.Abstractions.Feature.Task1.SubTask2;
 using Study.LabWork2.Abstractions.Feature.Task1.SubTask2.DtoModels;
 
@@ -6,9 +7,91 @@ namespace Study.LabWork2.Feature.Task1.SubTask2;
 /// <summary>
 /// Определяет реализацию для процессора наборов чисел
 /// </summary>
-public sealed class NumberSetProcessor : INumberSetProcessor
+public sealed class NumberSetProcessor(List<int[]> dataSets,
+    int maxThreadCount) : INumberSetProcessor
 {
-    public ProcessingResultDto GetResult() => throw new NotImplementedException();
+    private readonly List<int[]> _dataSets = dataSets;
+    private readonly List<ResultEntryDto> _results = [];
+    private int _totalSum = 0;
+    private readonly object _locker = new();
+    private readonly Mutex _mutex = new();
+    private readonly Semaphore _semaphore = new(maxThreadCount, maxThreadCount);
+    private TimeSpan _execTime = TimeSpan.Zero;
 
-    public void Process() => throw new NotImplementedException();
+
+
+    public void Process()
+    {
+        var stopwatch = Stopwatch.StartNew();
+        var threads = new List<Thread>();
+
+        for (int i = 0; i < _dataSets.Count; i++)
+        {
+            int setNumber = i + 1;
+            int[] data = _dataSets[i];
+
+            var thread = new Thread(() => ProcessDataSet(setNumber, data));
+            threads.Add(thread);
+            thread.Start();
+        }
+
+        foreach (var thread in threads)
+        {
+            thread.Join();
+        }
+
+        stopwatch.Stop();
+        _execTime = stopwatch.Elapsed;
+    }
+
+    private void ProcessDataSet(int setNumber, int[] data)
+    {
+        _semaphore.WaitOne();
+
+        try
+        {
+            int threadId = Thread.GetCurrentProcessorId();
+            int sum = 0;
+
+            foreach (int num in data)
+            {
+                sum += num;
+            }
+
+            lock (_locker)
+            {
+                _results.Add(new ResultEntryDto
+                {
+                    SetNumber = setNumber,
+                    Sum = sum,
+                    ThreadId = threadId
+                });
+            }
+
+            _mutex.WaitOne();
+            try
+            {
+                _totalSum += sum;
+            }
+            finally
+            {
+                _mutex.ReleaseMutex();
+            }
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
+    }
+
+    public ProcessingResultDto GetResult()
+    {
+        return new ProcessingResultDto
+        {
+            Results = _results,
+            TotalSum = _totalSum,
+            ProcessedSetsCount = _results.Count,
+            ExecutionTime = _execTime
+        };
+    }
 }
